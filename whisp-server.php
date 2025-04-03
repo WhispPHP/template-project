5b24801b4b1a0aa9ad5fc765f2c2be6d57079a42
Ashley Hindle <hey@ashleyhindle.com>
2025-04-03 11:48:42 +0100

    update Whisp to 1.2.5 - update our script to use auto discovery

--- whisp-server.php
+++ whisp-server.php
@@ -6,14 +6,3 @@ require_once __DIR__ . '/vendor/autoload.php';
 (new Whisp\Server(port: 2020))
     ->setLogger(new Whisp\Loggers\FileLogger(__DIR__ . '/server.log'))
     ->run(); // Auto discovers apps from the 'apps' directory
-
-/*
-'default' => __DIR__ . '/apps/prompts.php',
-'howdy' => __DIR__ . '/apps/howdy-world.php',
-'howdy-{name}' => __DIR__ . '/apps/howdy-world.php',
-'mouse' => __DIR__ . '/apps/mouse.php',
-'confetti' => __DIR__ . '/apps/confetti.php',
-'beep' => __DIR__ . '/apps/beep.php', // ghostty doesn't support BEL
-'clipboard-button' => __DIR__ . '/apps/clipboard-button.php',
-'clipboard' => __DIR__ . '/apps/clipboard-prompts.php',
-*/
